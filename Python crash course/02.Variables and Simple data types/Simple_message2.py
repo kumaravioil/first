c19cbc7c79d3e6ffd1e39933aab2897f7f8aa8da
kumaravioil <kumar.avioil@gmail.com>
2025-05-31 21:02:54 +0530

    practice

---
+++ Python crash course/02.Variables and Simple data types/Simple_message2.py
@@ -0,0 +1,24 @@
+msg="Kumar"
+print(msg)
+msg="pushpa"
+print(msg)
+msg1="kumar"
+print(msg1.upper())
+print(msg1.lower())
+print(msg1.title())
+first_name='ThangaPushpam'
+last_name="Kumar"
+full_name=f'{first_name} {last_name}'
+print(full_name)
+print(f'Hello,{full_name.title()}!')
+print("Languages:\n\tC\n\tPython\n\tJava")
+aircraft="   Rafale    "
+print(aircraft)
+print(aircraft.strip())
+print(aircraft.rstrip())
+print(aircraft.lstrip())
+url="http://kumar.com"
+print(url.removeprefix("http://"))
+print(url.removesuffix(".com"))
+
+
---
+++ Python crash course/02.Variables and Simple data types/Simple_message3.py
@@ -0,0 +1,21 @@
+# These are the code practice to be done in Chapter 2 of Python Crash Course
+name="Eric"
+print(f'Hello {name}, Would you like to learn some Python today?')
+print(name.title())
+print(name.upper())
+print(name.lower())
+print('Albert Einstein once said,\n\t' 
+'"A person who never made a mistake never tried anything new"')
+scientist="Albert Einstein"
+print(f'{scientist} once said,\n\t'
+'"A person who never made a mistake never tried anything new"')
+UNIVERSE_AGE=14_000_000_000
+print(UNIVERSE_AGE)
+x,y,z=5,6,7
+print(x)
+print(z)
+print(4+4)
+print(16-8)
+print(4*2)
+print(16/2)
+
