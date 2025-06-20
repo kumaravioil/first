4ddf83c315f47686d424919915144f53787fe72a
kumaravioil <kumar.avioil@gmail.com>
2025-06-20 20:47:50 +0530

    practice

---
+++ Python crash course/08.Functions/defarg.py
@@ -0,0 +1,8 @@
+def add(*kumar):
+    add=0
+    for i in kumar:
+        add=add+i
+    return add
+
+sum=add(1,2,3,4)
+print(sum)
--- Python crash course/08.Functions/formattedname.py
+++ Python crash course/08.Functions/formattedname.py
@@ -8,12 +8,21 @@ print(director)
 print(formated_name("Kumar","Ramasamy"))
  """
 
-def fullname(first_name,last_name,middle_name=""):
+""" def fullname(first_name,last_name,middle_name=""):
     if middle_name:
         full_name=f'{first_name} {middle_name} {last_name}'
     else:
         full_name=f'{first_name} {last_name}'
     return full_name
 
-name=fullname("Kumar","Ramasamy")
-print(name)
+name=fullname("Kumar","Ramasamy","sant")
+print(name) """
+
+def city_country(city,country):
+    name=f'{city.upper()},{country.upper()}'
+    return name
+
+print(city_country("Delhi","India"))
+print(city_country("Paris","France"))
+print(city_country("Moscow","Russia"))
+
